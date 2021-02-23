8c0db602020f2123f1dc49529ecdf080990784ed
Marketionist <marketionist@gmail.com>
2021-02-22 19:01:56 -0500

    Add tests for 'user clicks' step

--- tests/page-objects/test1-page.js
+++ tests/page-objects/test1-page.js
@@ -4,7 +4,9 @@
 
 let test1Page = {
 
-    pageTest1: 'http://localhost:8001/test1.html'
+    pageTest1: 'http://localhost:8001/test1.html',
+    linkTest2Page: '#link-test2-page',
+    linkTest2PageXPath: '//*[@id="link-test2-page"]'
 
 };
 
